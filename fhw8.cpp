57efc75ce22a5d19a16d3917bf17cc0caca62764
Joel <joeldholm@gmail.com>
2019-04-26 11:47:43 -0400

    seperated hw8 into two files

--- fhw8.cpp
+++ fhw8.cpp
@@ -26,37 +26,36 @@ int main(){
   char uPathc[uPath.size() + 1];
   strcpy(uPathc, uPath.c_str() );
 
-
-  //start time with open,read,write,close
-  clock_t startTime = clock();
+  //Now do it again but with fopen, fread, fwrite, and fclose
+  clock_t fstartTime = clock();
 
   //open both files
-  int lfd = open(lPathc, O_RDONLY);
-  int ufd = open(uPathc, O_WRONLY);
+  FILE *flfd = fopen(lPathc, "r");
+  FILE *fufd = fopen(uPathc, "w");
 
   //read lower case file
-  struct stat lbuf;
-  fstat(lfd,&lbuf);
-  size_t lSize = lbuf.st_size;
-  char lFileContent[lSize];
-  read(lfd,lFileContent,lSize);
+  struct stat flbuf;
+  fstat(fileno(flfd),&flbuf);
+  size_t flSize = flbuf.st_size;
+  char flFileContent[flSize];
+  fread(flFileContent,1, flSize, flfd);
+
 
   //convert to upper case
   int i = 0;
-  for( ; i < lSize; i++){
-    lFileContent[i] = toupper(lFileContent[i]);
+  for( ; i < flSize; i++){
+    flFileContent[i] = toupper(flFileContent[i]);
   }
 
   //write to upper case file
-  write(ufd,lFileContent,lSize);
+  fwrite(flFileContent,flSize, 1, fufd);
 
-  close(lfd);
-  close(ufd);
+  fclose(flfd);
+  fclose(fufd);
 
-  clock_t endTime = clock();
-  clock_t totalTime = endTime - startTime;
-  cout << "open, read, write, and close Total Time: " << totalTime << endl;
-
+  clock_t fendTime = clock();
+  clock_t ftotalTime = fendTime - fstartTime;
+  cout << "fopen, fread, fwrite, and fclose Total Time: " << ftotalTime << endl;
 
   return 0;
 }
